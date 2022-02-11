ad87c2fe2f4273d5683f5a571ab21950348ee22c
Aditya <53331534+addy123d@users.noreply.github.com>
2022-02-11 13:31:52 +0530

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url : "mongodb+srv://aditya123d:B5YxLYCeapUBXYlH@onlinemarket.m36fm.mongodb.net/ONLINEMARKET?retryWrites=true&w=majority",
+    url : "",
     key : "secret"
 }
